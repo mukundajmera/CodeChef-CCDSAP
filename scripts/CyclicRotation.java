dfce9528ef87daa25e61d2e42ccf801f353404bc
mukundajmera <mukundajmera94@gmail.com>
2019-09-03 12:54:57 +0530

    adding cyclic rotation

---
+++ scripts/CyclicRotation.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        int [] B = new int[A.length];
+        for(int i = 0;i < A.length;i++){
+            B[((i + K) % A.length)] = A[i];    
+        }
+        return B;
+    }
+}
