0d75634e168978e56a17f6dd20def9c4159ab85f
mukundajmera <mukundajmera94@gmail.com>
2020-01-13 11:15:53 +0530

    adding HEADBOB

---
+++ scripts/HEADBOB.py
@@ -0,0 +1,11 @@
+#HEADBOB
+for _ in range(int(input())):
+    len = int(input())
+    s = input()
+    if ('I' in s):
+        print("INDIAN")
+    elif('Y' in s):
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
+
